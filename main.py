feb63944a53bba2b31c0b0db2d50f6891b92c2d5
wseavey <williamaseavey@gmail.com>
2024-10-24 22:08:09 -0400

    working rotation and movement

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
         player.update(dt)
         player.draw(screen)
         pygame.display.flip() 
--- player.py
+++ player.py
@@ -16,11 +16,9 @@ class Player(CircleShape):
     
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
-        pass
 
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
-        pass
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
@@ -29,3 +27,11 @@ class Player(CircleShape):
             self.rotate(dt)
         if keys[pygame.K_d]:
             self.rotate(-dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
